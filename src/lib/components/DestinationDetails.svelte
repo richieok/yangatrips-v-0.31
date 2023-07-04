e976fc400fa48444fb718e5372eeccbd0cfe5d7e
Adolphus Okoro <adolphusokoro@Adolphuss-MacBook-Air.local>
2023-07-04 22:56:28 +0100

    whatsapp prettier

--- src/lib/components/DestinationDetails.svelte
+++ src/lib/components/DestinationDetails.svelte
@@ -1,11 +1,9 @@
 <script>
-    import DestinationDetails from "$lib/components/DestinationDetails.svelte"
-    export let data;
-    let {description, message, pictures, name} = data
-    let imgBucketPrefix =
-        "https://s3.eu-west-3.amazonaws.com/yangatrips.com/images/";
-    // console.log(data);
-    // $: ({description, message, pictures, name} = $page.data)
+    import {onMount} from "svelte"
+    export let description
+    export let message
+    export let imgsrc
+    export let alt
 
     let style = `<style>.markup p { font-size: .9em; line-height: 1.2rem}
                         .markup li { font-size: .8rem}
@@ -14,18 +12,29 @@
                 </style>`
 </script>
 
-<!-- {@html style} -->
+{@html style}
 
-<div class="page">
-    <DestinationDetails {description} imgsrc={`${imgBucketPrefix}${name}/${pictures[0]}`} {message} alt="panaroma"/>
+<div class="content">
+    <div class="img-display">
+        <img
+            src={imgsrc}
+            alt={alt}
+        />
+    </div>
+    <div class="tour-details">
+        <div class="mini-menu">
+            <a href="/#">Package Description</a>
+            <a href="/#">Locations</a>
+            <a href="/#">Book this tour</a>
+        </div>
+        <h3>{message}</h3>
+        <div class="markup">
+            {@html description}
+        </div>
+    </div>
 </div>
 
 <style>
-    .page {
-        padding: 5px;
-        min-height: clamp(500px, calc(100vh - var(--nav-height-mobile)), 800px);
-        animation: pageFade 1s forwards;
-    }
     .content {
         display: grid;
         grid-template-columns: 1fr;
@@ -50,9 +59,6 @@
         text-align: center;
         padding: 20px;
     }
-    .tour-details p {
-        font-size: .9em;
-    }
     .mini-menu {
         display: flex;
         justify-content: left;
@@ -74,14 +80,6 @@
         color: hsla(240, 74%, 17%, 0.965);
         font-weight: 900;
         line-height: 1.1rem;
-    }
-    @keyframes pageFade {
-        0% {
-            opacity: 0.1;
-        }
-        100% {
-            opacity: 1;
-        }
     }
     @media (min-width: 810px){
         .content {
