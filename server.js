d62da6ccd9e847cdbde0a2c75ddf244d5e575739
Adolphus Okoro <adolphusokoro@Adolphuss-MacBook-Air.local>
2023-07-16 20:51:40 +0100

    server takes PORT number argument

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ dotenv.config()
 import { handler } from './build/handler.js';
 import express from 'express';
 
+const p = process.argv.slice(2)
 console.log(process.env.PORT);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = +p || 3000;
 
 try{
     app.use(handler);
